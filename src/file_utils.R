a0d9882e318a29cb14a8b23f7fcc94ba720f3f92
limnoliver <soliver@usgs.gov>
2021-05-05 07:37:08 -0500

    add spatial data

--- src/file_utils.R
+++ src/file_utils.R
@@ -5,14 +5,13 @@ sf_to_zip <- function(zip_filename, sf_object, layer_name){
   on.exit(setwd(cdir))
   dsn <- tempdir()
   
-  sf::st_write(sf_object, dsn = dsn, layer = layer_name, driver="ESRI Shapefile", delete_dsn=TRUE) # overwrites
+  sf::st_write(st_as_sf(sf_object), dsn = dsn, layer = layer_name, driver="ESRI Shapefile", delete_dsn=TRUE) # overwrites
   
   files_to_zip <- data.frame(filepath = dir(dsn, full.names = TRUE), stringsAsFactors = FALSE) %>%
     mutate(filename = basename(filepath)) %>%
     filter(str_detect(string = filename, pattern = layer_name)) %>% pull(filename)
-  
   setwd(dsn)
-  zip(file.path(cdir, zip_filename), files = files_to_zip)
+  zip::zip(file.path(cdir, zip_filename), files = files_to_zip)
   setwd(cdir)
 }
 
@@ -26,4 +25,13 @@ reduce_metadata <- function(in_file, out_file) {
   
   write_csv(dat, out_file)
   
+}
+
+create_sf <- function(in_file) {
+  readRDS(in_file) %>%
+    select(site_id, source, longitude, latitude) %>%
+    st_as_sf(coords = c('longitude', 'latitude'), crs = 4326, remove = FALSE) %>%
+    as_Spatial() %>%
+    return()
+    
 }
